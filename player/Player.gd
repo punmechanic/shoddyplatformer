79806de4bf84e01f3e7aeedde4edca33e77d8cf6
Dan <dan@aredherring.tech>
2023-12-18 20:41:55 -0800

    add a jumping animation

--- player/Player.gd
+++ player/Player.gd
@@ -12,11 +12,12 @@ func _physics_process(delta: float):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	var is_jumping: bool = false
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		$Sprite.play("jump")
 		velocity.y = JUMP_VELOCITY
-		# TODO: Stop the jumping animation once the player lands on the floor.
+		is_jumping = true
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -26,11 +27,18 @@ func _physics_process(delta: float):
 		# Only change the facing direction if the player actually changed how they are facing.
 		# This prevents the sprite "snapping" back to facing left after the player stops pressing the right direction.
 		$Sprite.flip_h = direction > 0
+		# TODO: Don't play the walking animation when the player is jumping./
 		$Sprite.play("walk")
 	else:
 		# Player is stood still.
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		$Sprite.play("idle")
 
 	move_and_slide()
 	
+	# If the player is not jumping AND the player has no direction, the player is stood still and should idle.
+	# We still want the walking animation to play if the player has a direction even if the player can't move.
+	#
+	# This is done after move_and_slide() because move_and_slide() will change the state of is_on_floor() to reflect
+	# any jumps that occurred during this frame.
+	if is_on_floor() and direction == 0:
+		$Sprite.play("idle")
