aa679741e287831b637b06150728b3465e1b2234
Dan <dan@aredherring.tech>
2023-12-18 20:27:52 -0800

    Fix walking animation

--- player/Player.gd
+++ player/Player.gd
@@ -7,7 +7,6 @@ extends CharacterBody2D
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-
 func _physics_process(delta: float):
 	# Add the gravity.
 	if not is_on_floor():
@@ -15,7 +14,9 @@ func _physics_process(delta: float):
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		$Sprite.play("jump")
 		velocity.y = JUMP_VELOCITY
+		# TODO: Stop the jumping animation once the player lands on the floor.
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -25,7 +26,11 @@ func _physics_process(delta: float):
 		# Only change the facing direction if the player actually changed how they are facing.
 		# This prevents the sprite "snapping" back to facing left after the player stops pressing the right direction.
 		$Sprite.flip_h = direction > 0
+		$Sprite.play("walk")
 	else:
+		# Player is stood still.
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		$Sprite.play("idle")
 
 	move_and_slide()
+	
